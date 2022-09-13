5675850f704b5ab8c2bcf0b967b7efb7f1f95a2f
KhabibullaevAli <khabibullaevali@gmail.com>
2022-09-13 13:21:24 +0300

    Complete

--- src/main/java/web/service/UserService.java
+++ src/main/java/web/service/UserService.java
@@ -1,36 +1,18 @@
 package web.service;
 
-import org.springframework.stereotype.Service;
-import web.dao.UserDao;
 import web.model.User;
 import java.util.List;
 
-@Service
-public class UserService {
-    private final UserDao userDao;
+public interface UserService {
 
-    public UserService(UserDao userDao) {
-        this.userDao = userDao;
-    }
+   public List<User> getUserList();
 
-    public List<User> getUserList() {
-        return userDao.getUserList();
-    }
+    public User getUserById(int id);
 
-    public User getUserById(int id) {
-        return userDao.getUserById(id);
-    }
+    public void save(User user);
 
-    public void save(User user) {
-        userDao.save(user);
-    }
+    public void update(int id, User user);
 
-    public void update(int id, User user) {
-        userDao.update(id, user);
-    }
-
-    public void delete(int id) {
-        userDao.delete(id);
-    }
+    public void delete(int id);
 
 }
